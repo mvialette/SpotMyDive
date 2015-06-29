fafeabdfbe8af92d4e85c0f9831f75d518d9fe96
Maxime VIALETTE <maxime.vialette@gmail.com>
2015-06-29 22:50:33 +0200

    Merge content of http://codepen.io/mvialette/pen/PqJPGO to this git repo.

--- platforms/android/assets/www/assets/javascripts/app.js
+++ platforms/android/assets/www/assets/javascripts/app.js
@@ -3,6 +3,20 @@
 //var app = angular.module('SpotMyDiveApp', ['ngMaterial', 'spots']);
 var app = angular.module('SpotMyDiveApp', ['ngMaterial', 'SpotMyDiveControllers', 'SpotMyDiveServices']);
 
+app.filter('deepBetween', function(){
+  return function(items, min, max) {
+   // alert(min);
+   // alert(max);
+      var filtered = [];
+      angular.forEach(items, function(item, key) {
+        if(item.profondeurMax <= max && item.profondeurMax >= min) {
+            filtered.push(item);
+          }
+      });
+      return filtered;
+  };
+});
+/*
 app.config(function($mdThemingProvider) {
 
     // Use the 'brown' theme - override default 'blue' theme
@@ -12,3 +26,4 @@ app.config(function($mdThemingProvider) {
 
 });
 
+*/
